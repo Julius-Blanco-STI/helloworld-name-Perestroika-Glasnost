42014e02fc3b90039f4d9f5ab1dd19982971fa64
Perestroika-Glasnost <58575874+Perestroika-Glasnost@users.noreply.github.com>
2019-12-06 09:10:58 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,7 +1,18 @@
+package main;
+import java.util.*;
 public class Main {
 
-  public static void main(String[] args) {
-    System.out.println("Hello World");
-  }
-
+    public static void main(String[] args) {
+     Scanner scan = new Scanner (System.in);
+     System.out.println("Hello World!");
+     System.out.println("What's your name? ");
+     String name = scan.nextLine();
+     System.out.println("How old are you? ");
+     int age = scan.nextInt();
+     System.out.print("What's your address? ");
+     String address = scan.nextLine();
+     System.out.println("Hi "+name+"!");
+     System.out.println("You are "+ age + "years old!");
+     System.out.println("You live at" + address);
+    }
 }
